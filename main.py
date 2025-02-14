555a8ff3ffe2bc4b85f3046047b87f3c73b9f6a5
SKEIILATT <javimacias619@gmail.com>
2025-02-14 18:12:54 -0500

    Add the principal window

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello word :D")
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("Analysis Dashboard")
+root.geometry("500x500")
+label_db = tk.Label(root, text="Enter your credentials to connect your database")
+label_db.pack()
+
+
+
+
+
+
+root.mainloop()
+
+
+
+
